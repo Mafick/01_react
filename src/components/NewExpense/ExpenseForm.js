61848d5ba8fca2d6403e36e744799cda0bcb60e8
Niels Horn <niels.horn@smartcommerce.de>
2023-02-16 13:10:49 +0100

    two way binding

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -47,22 +47,50 @@ const ExpenseForm = () => {
 		*/
 	}
 
+	const submitHandler = (event) => {
+		event.preventDefault();
+
+		const expenseData = {
+			title: enteredTitle,
+			amount: enteredAmount,
+			date: new Date(enteredDate)
+		};
+		console.log(expenseData);
+
+		setEnteredTitle('');
+		setEnteredAmount('');
+		setEnteredDate('');
+	};
+
 	return (
-		<form>
+		<form onSubmit={submitHandler}>
 			<div className="new-expense__controls">
 				<div className="new-expense__control">
 					<label>Title</label>
-					<input type='text' onChange={titleChangeHandler}></input>
+					<input type='text'
+					       value={enteredTitle}
+					       onChange={titleChangeHandler}
+					/>
 				</div>
 
 				<div className="new-expense__control">
 					<label>Amount</label>
-					<input type='number' min="0.01" step="0.01" onChange={amountChangeHandler}></input>
+					<input type='number'
+					       min="0.01"
+					       step="0.01"
+					       value={enteredAmount}
+					       onChange={amountChangeHandler}
+					/>
 				</div>
 
 				<div className="new-expense__control">
 					<label>Date</label>
-					<input type='date' min="2019-01-01" max="2023-12-31" onChange={dateChangeHandler}></input>
+					<input type='date'
+					       min="2019-01-01"
+					       max="2023-12-31"
+					       value={enteredDate}
+					       onChange={dateChangeHandler}
+					/>
 				</div>
 			</div>
 
